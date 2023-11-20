c80e7e76bd7931f2bf711269e0a91f7814faa16d
Phurin Vanasrivilai <phurinjeffyjj@gmail.com>
2023-11-21 03:29:14 +0700

    navbar basic

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,10 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
-      <div className="text-center">
-        TEST
-      </div>
+      <Navbar />
     </>
   );
 }
